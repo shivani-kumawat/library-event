7fc2b046cfee0e0744c592c7222f7cf004adbb63
= <=>
2023-04-11 16:38:40 +0530

    updated

--- library-event-consumer/src/main/java/com/learnkafka/libraryeventconsumer/config/LibraryEventsConsumerConfig.java
+++ library-event-consumer/src/main/java/com/learnkafka/libraryeventconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,24 @@
 package com.learnkafka.libraryeventconsumer.config;
 
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+  // this bean can override or add new configurations
+  @Bean
+  ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+      ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+      ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    configurer.configure(factory, kafkaConsumerFactory);
+    factory.setConcurrency(3); //kafka consumer with 3 threads
+    // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // to set manual mode
+    return factory;
+  }
 }
